1119ddcdca73abeb0fd0d71e3c27565d4a2b480d
Rohit Purty <55530094+raymondM5GT@users.noreply.github.com>
2020-11-19 13:58:17 +0530

    Delete colors.py

--- colors.py
+++
@@ -1,8 +0,0 @@
-from collections import namedtuple
-
-color = namedtuple("color", ["red", "green", "blue"])
-BG_color = color(red=60, green=144, blue=80)
-SNAKE = color(red=255, green=200, blue=100)
-FOOD = color(red=200, green=188, blue=69)
-TEXT = color(red=255, green=95, blue=165)
-
